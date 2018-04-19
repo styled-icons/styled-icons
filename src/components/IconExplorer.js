230f4475b75007c3788380710f1476dd7f9f94da
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-04-18 20:43:24 -0500

    Add search placeholder text, limit searching to

--- src/components/IconExplorer.js
+++ src/components/IconExplorer.js
@@ -9,19 +9,25 @@ export class IconExplorer extends React.PureComponent {
 
   updateSearch = event => {
     const search = event.target.value
-    this.setState({search})
+
+    if (search.length === 0 || search.length > 2) {
+      this.setState({search})
+    }
   }
 
   render() {
-    const icons = this.state.icons
-
     const filteredIcons = this.state.search
       ? this.props.search.search(this.state.search)
       : this.props.icons
 
     return (
       <div>
-        <input className="search-box" type="text" onChange={this.updateSearch} />
+        <input
+          className="search-box"
+          type="text"
+          onChange={this.updateSearch}
+          placeholder="search icons"
+        />
 
         <div className="icons-container">
           {filteredIcons.map(({importPath, icon, name, pack}) => (
