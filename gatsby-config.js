c649a9e1c959be1cdf8c926a0d76f1a60e16c832
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-04-25 17:46:55 -0500

    Add Gatsby favicon plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,24 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: 'src/images/styled-icons.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false,
+        },
+      },
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: 'Styled Icons',
