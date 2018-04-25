04123e2605d1106a4f6c5fd76c7ac44ddc8ebe1c
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-04-25 17:29:21 -0500

    Add site manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Styled Icons - a Styled Components icon library',
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-netlify'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Styled Icons',
+        short_name: 'Styled Icons',
+        start_url: '/',
+        background_color: '#db7093',
+        theme_color: '#db7093',
+        display: 'minimal-ui',
+        icon: 'src/images/styled-icons.png',
+      },
+    },
+    'gatsby-plugin-netlify',
+  ],
 }
