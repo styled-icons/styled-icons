6fe535c123052d3ea6fc9ffe9c926ef7a8435bea
Jensderond <jensderond@gmail.com>
2020-03-06 12:06:58 +0100

    fix: order icon packages
    Signed-off-by: Jensderond <jensderond@gmail.com>

--- packages/website/src/IndexPage.tsx
+++ packages/website/src/IndexPage.tsx
@@ -29,8 +29,8 @@ const IndexPage: React.SFC = () => (
         <a href="https://fontawesome.com/">Font Awesome</a>,{' '}
         <a href="https://zurb.com/playground/foundation-icon-fonts-3">Foundation</a>,{' '}
         <a href="https://github.com/refactoringui/heroicons">Heroicons</a>, <a href="https://icomoon.io">Icomoon</a>,{' '}
-        <a href="https://material.io/icons/">Material Design</a>, <a href="https://octicons.github.com/">Octicons</a>,{' '}
-        <a href="https://www.npmjs.com/package/open-iconic">Open Iconic</a>, <a href="https://ionicons.com">Ionicons</a>,{' '}
+        <a href="https://ionicons.com">Ionicons</a>, <a href="https://material.io/icons/">Material Design</a>,{' '}
+        <a href="https://octicons.github.com/">Octicons</a>, <a href="https://www.npmjs.com/package/open-iconic">Open Iconic</a>,{' '}
         <a href="https://www.s-ings.com/typicons/">Typicons</a>, <a href="https://www.zondicons.com/">Zondicons</a>
       </p>
 
