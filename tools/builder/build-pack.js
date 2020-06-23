2b3d4efda7568a7c448bdf814b7d8067e68cafa8
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-06-23 21:02:37 +0100

    Fix regex

--- tools/builder/build-pack.js
+++ tools/builder/build-pack.js
@@ -24,7 +24,7 @@ const SVG_ATTRS = [
 ]
 
 const getComponentName = originalName => {
-  originalName = originalName.replace(/^\d+/, digits => `${toWords(parseInt(digits, 10)).replace(/[^\w\s]/, '')}_`).replace(/_$/, '')
+  originalName = originalName.replace(/^\d+/, digits => `${toWords(parseInt(digits, 10)).replace(/[^a-zA-Z\s]/, '')}_`).replace(/_$/, '')
   return originalName.length === 1 ? originalName.toUpperCase() : fastCase.pascalize(originalName)
 }
 
