225853734c673d11d5a0936a974f7467e84d129d
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-07-17 01:31:27 +0100

    Build icons with esbuild

---
+++ tools/builder/esbuild.js
@@ -0,0 +1,49 @@
+const esbuild = require('esbuild')
+const fsp = require('fs').promises
+const fastGlob = require('fast-glob')
+
+async function build(input, output, format = 'cjs') {
+  const source = await fsp.readFile(input, {encoding: 'utf8'})
+
+  try {
+    const res = await esbuild.transform(source, {
+      format,
+      target: 'node12',
+      loader: 'tsx',
+    })
+    await fsp.writeFile(output, res.code)
+  } catch (err) {
+    console.log({input, output})
+    throw err
+  }
+}
+
+async function run() {
+  const tsFiles = await fastGlob(['packages/**/*.ts', 'packages/**/*.tsx'], {
+    ignore: ['packages/@styled-icons/styled-icon/**/*'],
+  })
+  const sources = tsFiles.filter((file) => !file.endsWith('.d.ts'))
+
+  const promises = []
+
+  promises.push(
+    ...sources.map(async (file) => {
+      const output = file.replace('.ts', '.js')
+      await build(file, output)
+    }),
+  )
+
+  promises.push(
+    ...sources.map(async (file) => {
+      const output = file.replace('.ts', '.esm.js')
+      await build(file, output, 'esm')
+    }),
+  )
+
+  await Promise.all(promises)
+}
+
+run().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
